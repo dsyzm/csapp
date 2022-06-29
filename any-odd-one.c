96b4210a10f6781284637ec45b25f412733b54cc
dsyzm <zm504024159@gmial.com>
2022-06-29 07:21:03 +0000

    correct one

--- any-odd-one.c
+++ any-odd-one.c
@@ -2,10 +2,10 @@
 #include<assert.h>
 int any_odd_one(unsigned x){
 	int num = 0xaaaaaaaa;
-	return !!(x & num)^num;
+	return !!(x & num);
 }
 int main(int argc, char *argv[]){
 	assert(any_odd_one(0xff));
-	assert(any_odd_one(0x2));
+	assert(any_odd_one(0x1));
 	return 0;
 }
